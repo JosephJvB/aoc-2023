b2967e64cb4cfb4121ba60dc49dda4ce02b2463d
Joe van Bohemen <joevanbo@protonmail.com>
2024-01-04 15:33:56 +0000

    my lcm attempt not working yet

--- src/day-8/8.test.ts
+++ src/day-8/8.test.ts
@@ -163,7 +163,6 @@ const multiTraverse = (network: Network) => {
   let i = 0
 
   let nodes = [...network.nodes.values()].filter((n) => n.id.endsWith('A'))
-  console.log(nodes.length, 'starting nodes')
 
   while (!!nodes.find((n) => !n.id.endsWith('Z'))) {
     const nextDirection = network.instructions[i]
@@ -188,6 +187,60 @@ const multiTraverse = (network: Network) => {
 
   return steps
 }
+
+const getNextNode = (network: Network) => (i: number, currentNode: Node) => {
+  const nextDirection = network.instructions[i]
+  const nextNode = network.nodes.get(currentNode[nextDirection])
+  if (!nextNode) {
+    throw new Error(
+      `no next node ${JSON.stringify({
+        nextDirection,
+        currentNode,
+      })}`
+    )
+  }
+
+  return nextNode
+}
+
+const getLowestMultiples = (network: Network) => {
+  let nodes = [...network.nodes.values()].filter((n) => n.id.endsWith('A'))
+  const startNodesLength = nodes.length
+
+  const multiples: number[] = []
+  let i = 0
+  let steps = 1
+
+  const nextNodeGetter = getNextNode(network)
+  while (multiples.length < startNodesLength) {
+    const nextNodes: Node[] = []
+    nodes.forEach((n) => {
+      const nextNode = nextNodeGetter(i, n)
+      if (nextNode.id.endsWith('Z')) {
+        multiples.push(steps)
+      } else {
+        nextNodes.push(nextNode)
+      }
+    })
+
+    steps++
+    i = i + 1 === network.instructions.length ? 0 : i + 1
+    nodes = nextNodes
+  }
+
+  return multiples
+}
+
+const gcd = (a: number, b: number): number => (!b ? a : gcd(b, a % b))
+
+const solveLcm = (multiples: number[]) => {
+  console.log(multiples)
+  const numerator = multiples.reduce((tot, n) => tot * n, 1)
+  const denominator = multiples.reduce(gcd)
+
+  return numerator / denominator
+}
+
 describe('day 8.2', () => {
   describe('test 8.2', () => {
     const FILENAME = '8.2-test-data.txt'
@@ -199,13 +252,38 @@ describe('day 8.2', () => {
 
       expect(steps).toBe(6)
     })
+
+    it('can find multiples for 8.2', () => {
+      const network = parseFile(FILENAME)
+
+      const multiples = getLowestMultiples(network)
+
+      expect(multiples.length).toBe(2)
+      expect(multiples).toEqual([2, 3])
+    })
+
+    it('can solve LCM [2, 3]', () => {
+      const input = [2, 3]
+
+      const result = solveLcm(input)
+
+      expect(result).toBe(6)
+    })
+
+    it('can solve LCM [23, 7]', () => {
+      const input = [23, 7]
+
+      const result = solveLcm(input)
+
+      expect(result).toBe(161)
+    })
   })
 
   describe('question 8.2', () => {
     const FILENAME = '8-data.txt'
 
     // use Lowest Common Multiple approach says reddit
-    it.skip('can solve test 8.2', () => {
+    it.skip('can solve question 8.2 naive', () => {
       const network = parseFile(FILENAME)
 
       const steps = multiTraverse(network)
@@ -215,6 +293,22 @@ describe('day 8.2', () => {
       })
 
       expect(steps).toBeGreaterThan(6)
+    })
+
+    it('can solve question 8.2 LCM', () => {
+      const network = parseFile(FILENAME)
+
+      // [ 11309, 12361, 13939, 16043, 18673, 19199 ] ?
+      // { answer2: 4.261200501047576e+22 } ??
+      const multiples = getLowestMultiples(network)
+
+      const result = solveLcm(multiples)
+
+      console.log({
+        answer2: result,
+      })
+
+      expect(result).toBeGreaterThan(6)
     })
   })
 })
